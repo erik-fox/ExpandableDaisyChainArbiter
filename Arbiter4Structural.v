57f189d8b18e97152f1d0968f75762233a2401e2
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-08 15:26:25 -0800

    structural compiled

--- Arbiter4Structural.v
+++ Arbiter4Structural.v
@@ -7,10 +7,9 @@ module Arbiter4(r,g);
 input [0:3]r;
 output[0:3]g;
 wire [0:2] c;
-reg cout;
 ArbiterCell C0(r[0],1'b1,g[0],c[0]);
 ArbiterCell C1(r[1],c[0],g[1],c[1]);
 ArbiterCell C2(r[2],c[1],g[2],c[2]);
-ArbiterCell C3(r[3],c[2],g[3],cout);
+ArbiterCell C3(r[3],c[2],g[3],);
 
 endmodule
