a5a3298addd58ef3444e7a84efca5181f499ace1
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-08 15:56:25 -0800

    updated testbench appears to work

--- Arbiter4TB.v
+++ Arbiter4TB.v
@@ -4,6 +4,22 @@
 //Four bit arbiter Testbench
 
 module top;
+reg [0:3]r=0;
+wire [0:3]g;
+
+Arbiter4 A0(r,g);
+initial
+begin
+$display("G0 G1 G3 G3");
+$monitor("%b %b %b %b",g[0],g[1],g[2],g[3]);
+end
+always
+begin
+#6;
+if(r==15)
+	$stop;
+r=r+4'b0001;
+end
 
 
 endmodule
