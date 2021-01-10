58006c3f72bf647e7d2dd16091e92d865f7eb758
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-10 14:11:25 -0800

    Working sim.. Test more

--- ArbiterNTB.v
+++ ArbiterNTB.v
@@ -7,20 +7,31 @@ module top();
 parameter n=8;
 reg [0:n-1]r=0;
 wire [0:n-1]g;
-reg counter=0;
+reg [0:n]test=1;
 ArbiterN #(n) A0(r,g);
+/*
 initial
 begin
 $monitor("%b  -  %d",r,g);
 end
-
+*/
 always
 begin
-#6;
-if( r==((1<<n)-1))
-	$stop;
-if(!(r<(g<<1)&&r>=g))
-	counter=counter+1; 
-r=r+1;
+#6
+	test=g<<1;
+	if( r==((1<<n)-1))
+	begin
+		$display("no error\n");
+		$stop;
+	end
+	if( g!=0)
+	begin
+		if(!(r<(test)&&r>=g))
+		begin
+			$display("Failure\n");
+			$stop;
+		end
+	end
+	r=r+1;
 end
 endmodule
