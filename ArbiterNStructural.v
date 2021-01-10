895d6952bcda9bf6d075156fe2b88e6df028efe9
Erik Fox <erfox@auto.ece.pdx.edu>
2021-01-09 18:38:35 -0800

    testable Nbit Arbiter code

--- ArbiterNStructural.v
+++ ArbiterNStructural.v
@@ -8,11 +8,15 @@ parameter n=8;
 input [0:n-1]r;
 output[0:n-1]g;
 wire [0:n-2]w;
-
+genvar i;
 generate
 for(i=0; i <n; i=i+1)
 	begin:ARBITER
-		ArbiterCell w (r[i] );
+		if(i==0)
+			ArbiterCell w (r[i], 1'b1, g[i],w[i]);
+		else
+
+			ArbiterCell w (r[i], w[i-1], g[i],w[i]);
 	end
 endgenerate
 
